91150a1fa4c9cd0169eb49ae714af7776bc2333e
Paul Martin Panez Caballero <polsc_paul@hotmail.com>
2013-11-17 19:14:17 -0500

    Update new.py

--- new.py
+++ new.py
@@ -1,12 +1,9 @@
 n = int(raw_input("Ingresar la Cantidad de Trabajadores: "))
 c = 1
-
 while c <= n:
-
     nombre = raw_input("Ingrese Nombre del Trabajador: ")
     opcion = int(raw_input("\n Menu\n\t" + "1.Barnizado\n\t 2.Laqueado\n\t 3.Pintado\n\t" + "Ingresar Cargo: "))
     hora1 = int(raw_input("Ingresar Horas Trabajadas Durante la Semana: "))
-
     hora = 40
     hora_x_opcion = {1:10,2:12,3:14}
     valido = False
@@ -24,8 +21,7 @@ while c <= n:
             break
         else:
             print "Ingresar Opciones Validas"
-            opcion = int(raw_input("\n Menu\n\t" + "1.Barnizado\n\t 2.Laqueado\n\t 3.Pintado\n\t" + "Ingresar Cargo :"))
+            opcion = int(raw_input("\n Menu\n\t" + "1.Barnizado\n\t 2.Laqueado\n\t 3.Pintado\n\t" + "Ingresar Cargo: "))
             hora1 = int(raw_input("Ingresar Horas Trabajadas Durante la Semana: "))
     c += 1
-
     print "El Nombre es: " + str(nombre) + "\n La Hora Extra es: " + str(HE) + "\n El Jornal Extra es: " + str(JE) + "\n El Jornal Semanal es: " + str(sueldo)
