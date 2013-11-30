d78718cbd998e3f8ef9a6e0db737d7f82efca272
Paul Martin Panez Caballero <polsc_paul@hotmail.com>
2013-11-30 08:10:01 -0500

    Create ventana.py
    Ventana hecha con Tkinter

---
+++ ventana.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+#encoding:utf-8
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Dj_System"
+__date__ ="$30/11/2013 08:02:39 AM$"
+
+from Tkinter import *
+
+if __name__ == "__main__":
+    v0 = Tk()
+    v0.mainloop()
