3ef2a26a1026aa0a9b365bc21aac3e1be9266b8d
Paul Martin Panez Caballero <polsc_paul@hotmail.com>
2014-01-30 21:28:09 -0500

    Create plus.py

---
+++ plus.py
@@ -0,0 +1,20 @@
+"""
+e.g.:
+sum_double(1, 2) -> 3
+sum_double(3, 2) -> 5
+sum_double(2, 2) -> 8
+"""
+
+#input
+n1 = int(raw_input('Enter an integer: '))
+n2 = int(raw_input('Enter an integer: '))
+
+#process
+def sum_double(a, b):
+    if a != b:
+        return a+b
+    else:
+        return 2*(a+b)
+
+#output
+print 'The result is: ' + str(sum_double(n1, n2))
