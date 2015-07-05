b9f7e61004fe3b08d9fa586f3f7f0540b9f473b1
Paul Martin Panez Caballero <polsc_paul@hotmail.com>
2015-07-04 20:14:41 -0500

    Update one_piece_downloader.py

--- one_piece_downloader.py
+++ one_piece_downloader.py
@@ -10,47 +10,49 @@ from utilities import *
 import os
 import urllib2
   
-def download():
-    url = "http://www.chia-anime.tv/watch/one-piece-episode-"+episode+"/"
+def download(episodio):
+    url = "http://www.chia-anime.tv/watch/one-piece-episode-"+episodio+"/"
     html = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
     iFrames = []
     for iframe in soup("iframe"):
         iFrames.append(iframe.extract()["src"])
     url = iFrames[2]
     html = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
     params = []
     for param in soup("param"):
         params.append(param.extract()["value"])
     video = params[1][182:237]
     os.system('PyAxel ' + video)
     if os.name == "posix":
-        os.system("mv " + video[34:] + " op" + episode + ".mp4")
+        os.system("mv " + video[34:] + " op" + episodio + ".mp4")
     elif os.name == "nt":
-        os.system("move " + video[34:] + " op" + episode + ".mp4")
+        os.system("move " + video[34:] + " op" + episodio + ".mp4")
 
 def main():
     clrscr()
-    episode = raw_input("Ingrese el episodio de One Piece a descargar: ")
+    episode = raw_input("Ingrese el episodio de One Piece a descargar: ")	
     url = "https://en.wikipedia.org/wiki/List_of_One_Piece_episodes_%28season_17%29"
     html = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
     th = soup.findAll("th", {"id":"ep"+episode})
     for tr in soup("tr"):
-        tag = tr.findAll("th")
-        if tag == th:
-            td = tr.findChildren("td")        
+	    tag = tr.findAll("th")
+	    if tag == th:
+		    td = tr.findChildren("td")
     fecha = str(td[1])[4:-5]
-    today = date.today() + timedelta(days=1)
-    today = today.strftime("%B %d, %Y")
-    if fecha == today:
-        if int(str(datetime.today().time())[:2]) >= 21:
-             download()
-        else:
-            print "Intente despues de las 9:00 p.m."
+    today = date.today() + timedelta(days = 1)
+    hoy = today.strftime("%B %d, %Y")
+    if today.day < 10:
+	    hoy = hoy[:hoy.index(',')-2] + hoy[hoy.index(',')-1:]
+    if fecha == hoy:
+		if int(str(datetime.today().time())[:2]) >= 21:
+			download(episode)
+		else:
+			print "Intente despues de las 9:00 p.m."
     else:
-        print "El episodio se estrenara en la fecha: " + fecha
+		print "El episodio se estrenara en la fecha: " + fecha		
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
